4309462cdec433c6d60d54fde116ab1c34b06880
alexdgoncalves <alex.goncalves@deverest.com.br>
2024-05-30 21:07:26 -0300

    add: cors api config

--- pages/api/posts.js
+++ pages/api/posts.js
@@ -1,6 +1,11 @@
 import dados from '../../dados.json';
 
 export default function handler(req, res) {
+  // Configurar cabeçalhos CORS
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
   const { page = 1 } = req.query;
   const itemsPerPage = 10;
 
--- pages/api/posts/[id].js
+++ pages/api/posts/[id].js
@@ -1,6 +1,11 @@
 import dados from '../../../dados.json';
 
 export default function handler(req, res) {
+  // Configurar cabeçalhos CORS
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
   const { id } = req.query;
   const post = dados.posts.find(post => post.id === parseInt(id));
 
